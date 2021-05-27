625b9ff30d2acdd15fb09388f55745d2e6b46697
Grant Dasher <gdasher@google.com>
2021-05-27 23:06:11 +0000

    Finally works e2e!!!!

--- curvewaveguide/run_elmer.sh
+++ curvewaveguide/run_elmer.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+HOSTFILE="/kube-openmpi/generated/hostfile"
+OUT_FOLDER="$(TZ='America/Los_Angeles' date +%Y-%m-%d-%H-%M-%S)"
+
 /usr/bin/mpiexec \
   -mca orte_keep_fqdn_hostnames true \
-  --hostfile /kube-openmpi/generated/hostfile \
+  -wdir /physics \
+  --hostfile $HOSTFILE \
   -n 4 -npernode 1 /usr/local/bin/ElmerSolver_mpi
 
-gsutil cp -r /physics/output gs://gcp-security-dev-curriculum-data/$(TZ='America/Los_Angeles' date +%Y-%m-%d-%H-%M-%S)
+for host in $(cat $HOSTFILE); do
+  ssh $host -- gsutil cp -n '/physics/output/*' "gs://gcp-security-dev-curriculum-data/${OUT_FOLDER}" || true
+done
 
